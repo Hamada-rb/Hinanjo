98cc86b69ec73364dce5221fa4f3af487801d5a9
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-10-04 03:57:58 +0900

    add Place

---
+++ src/components/Place.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+class Place extends React.Component {
+
+    render() {
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Title>{this.props.info.name}</Card.Title>
+                    <div className="card-body">
+                        <div>
+                            {this.props.info.address}
+                        </div>
+                        <div>
+                            TEL:{this.props.info.tel}
+                        </div>
+                        <div>
+                            備考:{this.props.info.info}
+                        </div>
+                    </div>
+                    <a href={`https://www.google.com/maps/dir/?api=1&origin=${this.props.info.location.latitude},${this.props.info.location.longitude}&destination=${this.props.lat},${this.props.lng}`} className="btn btn-outline-success" variant="primary" >お店までの道順を見る</a>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+export default Place;
