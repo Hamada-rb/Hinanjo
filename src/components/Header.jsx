54c3bbd18707f12d8f33c8f525d280231c98ef67
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-10-04 03:34:48 +0900

    add Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+class Header extends React.Component {
+    render() {
+        return (
+            <Navbar className="bg-primary">
+                <Navbar.Brand href="" className="text-white">浜田市緊急避難所アプリ</Navbar.Brand>
+            </Navbar>
+        );
+    }
+}
+
+export default Header;
