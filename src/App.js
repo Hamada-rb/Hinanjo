995a374c0653fe9809ec38a6206ee9b13d9ea112
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-10-04 03:38:06 +0900

    add About

--- src/App.js
+++ src/App.js
@@ -6,11 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 // import component's
 import Header from './components/Header';
+import About from './components/About';
 
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <div className="app">
+        <Header />
+        <About />
+      </div>
     );
   }
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+class About extends React.Component {
+    render() {
+        return (
+            <Jumbotron>
+                <h1>浜田市緊急避難所アプリへようこそ</h1>
+                <p>
+                    あなたの現在位置を使い、周辺の避難所を一覧で表示してくれるアプリです。
+                </p>
+                <p>
+                    また位置情報が有効になっていない場合は避難所を表示できません。その場合は位置情報を許可してから再度「避難所を探す」ボタンをクリックしてください。
+                </p>
+            </Jumbotron>
+        );
+    }
+}
+
+export default About;
