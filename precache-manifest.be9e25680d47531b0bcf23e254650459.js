f5a5f208ef52b3372c0d47b119a800278a464579
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-10-22 15:37:35 +0900

    Updates

---
+++ precache-manifest.be9e25680d47531b0bcf23e254650459.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20f2e60c19ecbf3e71d9c2ce470fcbc0",
+    "url": "/hinanjo/index.html"
+  },
+  {
+    "revision": "2ab62589bd451d99a6ff",
+    "url": "/hinanjo/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "4f4e4f615eb74ea7341d",
+    "url": "/hinanjo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ab62589bd451d99a6ff",
+    "url": "/hinanjo/static/js/2.ca3e00a1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/hinanjo/static/js/2.ca3e00a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f4e4f615eb74ea7341d",
+    "url": "/hinanjo/static/js/main.eb385e10.chunk.js"
+  },
+  {
+    "revision": "883dc2b14ee32d938f53",
+    "url": "/hinanjo/static/js/runtime-main.1a3d473a.js"
+  }
+]);
