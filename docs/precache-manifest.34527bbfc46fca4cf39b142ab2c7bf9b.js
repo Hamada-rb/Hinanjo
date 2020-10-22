dfd2cbd05a637f1a596557d722db557bc6a3ac96
S-H-GAMELINKS <gamelinks007@gmail.com>
2020-10-22 14:49:51 +0900

    add static pages

---
+++ docs/precache-manifest.34527bbfc46fca4cf39b142ab2c7bf9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57d5c9f3698046f7ffaa665aaacb439b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ab62589bd451d99a6ff",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "3f82fe8ee65a42f1b3ae",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ab62589bd451d99a6ff",
+    "url": "/static/js/2.ca3e00a1.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ca3e00a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f82fe8ee65a42f1b3ae",
+    "url": "/static/js/main.cd522f62.chunk.js"
+  },
+  {
+    "revision": "69911237c1d5129e044b",
+    "url": "/static/js/runtime-main.b9125ec9.js"
+  }
+]);
